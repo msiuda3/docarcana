ce6da57fb58d8d32326488d2e6e3fb97c5965b1f
msiuda <siudamarcin3@gmail.com>
2025-12-15 00:43:38 +0100

    added filePathParser

--- src/main/java/com/example/docarcana/service/FilePathParser.java
+++ src/main/java/com/example/docarcana/service/FilePathParser.java
@@ -1,13 +1,17 @@
 package com.example.docarcana.service;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+
 
 public class FilePathParser {
 
 
     // /invoices/january
     public List<String> parsePath(String filePath){
-        return new LinkedList<>();
+        String[] result = filePath.split("/");
+        return Arrays.stream(result, 1, result.length)
+                .toList();
     }
 }
---
+++ src/test/java/com/example/docarcana/FilePathParserTest.java
@@ -0,0 +1,31 @@
+package com.example.docarcana;
+
+
+import com.example.docarcana.service.FilePathParser;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FilePathParserTest {
+
+    private FilePathParser filePathParser;
+
+    @Test
+    public void shouldParsePathCorrectly(){
+        this.filePathParser = new FilePathParser();
+        List<String> expected = new ArrayList<>();
+        expected.add("invoices");
+        expected.add("january");
+        String input = "/invoices/january";
+
+        assertEquals(expected, filePathParser.parsePath(input));
+    }
+
+
+
+}
